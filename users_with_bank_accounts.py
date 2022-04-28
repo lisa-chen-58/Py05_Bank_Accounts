8d59de1941e11b14fdaf92cf6855b66fa4ea3101
Lisa <lisa.hl.chen@gmail.com>
2022-04-27 19:45:36 -0700

    correcting formulas

--- users_with_bank_accounts.py
+++ users_with_bank_accounts.py
@@ -1,56 +1,90 @@
 class BankAccount:
-    all_accounts=[]
+    all_checking=[]
     def __init__(self, int_rate=0.03, balance=0): 
-        self.int_rate=int_rate
-        self.balance=balance
-        BankAccount.all_accounts.append(self)
-
-    def deposit(self, amount):
-        self.balance += amount
-        return self
-
-    def withdraw(self, amount):
-        if self.balance < 0:
-            print('Insufficient funds: Charging a $5 fee')
-            self.balance -= 5
-            return self
-        else:
-            self.balance -= amount
-            return self
-
-    def display_account_info(self):
-        print(f"Balance: {self.balance}")
-
-    def yield_interest(self):
-        if self.balance < 0:
-            print('Insufficient funds')
-        else:
-            self.balance *= self.int_rate
+        self.int_rate = int_rate
+        self.balance = balance
+        BankAccount.all_checking.append(self)
 
     @classmethod
-    def all_balances(cls):
-        for account in cls.all_accounts:
-            account.display_account_info()
+    def checkings_all(cls):
+        sum = 0
+        for checking in cls.all_checking:
+            print(checking.balance, checking.int_rate)
+            sum+=1
+
 
 class User:
-    def __init__(self,name):
-        self.name = name
-        self.account = {
-            "checking" : BankAccount(.02,1000),
-            "savings" : BankAccount(.05,3000)
-        }
 
+    def __init__(self, first_name, last_name, checking_bal = 0, savings_bal = 0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.checking = BankAccount(0.03,checking_bal)
+        self.savings = BankAccount(0.04, savings_bal)
+
+    # Applying interest
+    def apply_interest(self,account):
+        if account == "checking":
+            if self.checking.balance > 0:
+                self.checking.balance += (self.checking.balance * self.checking.int_rate)
+            return self
+
+        elif account == "savings":
+            if self.savings.balance > 0:
+                self.savings.balance += (self.savings.balance * self.savings.int_rate)
+            return self
+
+
+    # User deposits money into checking
+    def deposit_checking(self, amount):
+        self.checking += amount
+        print(f"${amount} has been deposited")
+        return self
+        
+    # User deposits money into savings
+    def deposit_savings(self, amount):
+        self.savings += amount
+        print(f"${amount} has been deposited")
+        return self
+        
+    # User withdraws money from checking
+    def withdraw_checking(self, amount):
+        if self.checking.balance < amount:
+            print('Insufficient funds: Charging a $5 fee')
+            self.checking.balance -= 5
+            
+        else:
+            self.checking.balance -= amount
+        return self
+
+    # User withdraws money from savings
+    def withdraw_savings(self, amount):
+        if self.savings < amount:
+            print('Insufficient funds: Charging a $5 fee')
+            self.savings -= 5
+            
+        else:
+            self.savings -= amount
+        return self
+
+    # Transfer checking to savings
+    def transfer(self,user,amount):
+        self.checking.balance -= amount
+        user.checking.balance += amount
+        print(f"{self.first_name} transferred ${amount} to {user.first_name}")
+        return self
+
+    # User Transfer
+    def user_transfer(self, user, amount):
+        self.checking.balance -= amount
+        user.checking.balance += amount
+        print(f"{self.first_name} has transferred ${amount} to {user.first_name}")
+        return self
+
+    # Display Balances
     def display_user_balance(self):
-        # print(f"{self.name}'s checking balance is {self.account['checking'].display_account_info()} and savings balance is {self.account['savings']}")
-        # return self
-        print(f"User: {self.name}, Checking Balance: {self.account['checking'].display_account_info()}")
-        print(f"User: {self.name}, Savings Balance: {self.account['savings'].display_account_info()}")
+        print(f"You have ${self.checking.balance} in your Checkings Account and ${self.savings.balance} in your savings account")
+        return self
 
-lisa = User("Lisa")
-saria = User("Saria")
-lisa.account['checking'].deposit(100)
-saria.account['savings'].deposit(2000)
-lisa.display_user_balance()
-saria.display_user_balance()
-
-
+lisa = User("Lisa","Chen", 1000,5000)
+saria = User("Saria","Dog","500","1000")
+lisa.display_user_balance().apply_interest("savings").display_user_balance()
